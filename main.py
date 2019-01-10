aa70034fbb5e754ce6f9afc566cf5d5a21a7cebb
BestCoderz <utk.bkkg@gmail.com>
2019-01-10 18:14:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -101,6 +101,20 @@ async def meme(ctx):
             embed.timestamp = datetime.datetime.utcnow()
             await client.say(embed=embed)
         
+	
+@client.command(pass_context=True)
+async def tweet(ctx, usernamename:str, *, txt:str):
+    url = f"https://nekobot.xyz/api/imagegen?type=tweet&username={usernamename}&text={txt}"
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get(url) as r:
+            res = await r.json()
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+            embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+            embed.set_image(url=res['message'])
+            embed.title = "{} twitted: {}".format(usernamename, txt)
+            await client.say(embed=embed)
+
+        
 @client.command(pass_context = True)
 async def help(ctx):
     if ctx.message.author.bot:
