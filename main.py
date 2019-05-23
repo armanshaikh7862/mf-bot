19c9b2cad2c1d3defc70cbe31ca77e6f79da180e
BestCoderz <utk.bkkg@gmail.com>
2019-05-23 13:44:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -114,6 +114,186 @@ async def tweet(ctx, usernamename:str, *, txt:str):
             embed.title = "{} twitted: {}".format(usernamename, txt)
             await client.say(embed=embed)
 
+@client.command(pass_context=True, no_pm=True, aliases=["r", "r34", "rule"])
+@commands.cooldown(3, 5)
+async def rule34(ctx, *, message:str=None):
+	if ctx.message.channel.is_nsfw == False:
+		embed=discord.Embed(description = "This is not a **nsfw** channel", color = color_blue)
+		x = await client.say(embed=embed)
+		await asyncio.sleep(5)
+		return await client.delete_message(x)
+	limit = 100
+	if message==None:
+		listu = ["anime", "ass", "boobs", "anal", "pussy", "thighs", "yaoi", "yuri", "bdsm"]
+		message = listu[random.randint(0, len(listu)-1)]
+	message = message.replace(" ", "_")
+	url = "http://rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&limit={}&tags={}".format(limit, message)
+	try:
+		response = requests.get(url)
+		data = json.loads(response.text)
+		limit = len(data)
+	except json.JSONDecodeError:
+		embed=discord.Embed(description = "Couldn't find a picture with that tag or there was a server problem", color = color_blue)
+		x = await client.say(embed=embed)
+		await asyncio.sleep(5)
+		return await client.delete_message(x)
+	x = data[random.randint(0, limit-1)]
+	final_url = "http://img.rule34.xxx/images/{}/{}".format(x["directory"], x["image"])
+	embed=discord.Embed(title = "Enjoy {}, lewd!!!".format(ctx.message.author.name), color = color_blue)
+	embed.set_image(url = final_url)
+	embed.set_footer(text = "From Rule34, Tag: {}, Results found: {}".format(message, limit))
+	await client.say(embed=embed)
+
+@client.command(pass_context=True, no_pm=True, aliases=["yan"])
+@commands.cooldown(3, 5)
+async def yandere(ctx, *, message:str=None):
+	if ctx.message.channel.is_nsfw == False:
+		embed=discord.Embed(description = "This is not a **nsfw** channel", color = color_blue)
+		x = await client.say(embed=embed)
+		await asyncio.sleep(5)
+		return await client.delete_message(x)
+	limit = 100
+	if message==None:
+		listu = ["pantsu", "swimsuits", "dress", "breasts", "animal ears", "open shirt", "bra", "no bra", "cameltoe", "loli"\
+				" thighhighs", "cleavage", "nipples", "ass", "bikini", "naked", "pussy", "panty pull", "see through", "underboob"]
+		message = listu[random.randint(0, len(listu)-1)]
+	message = message.replace(" ", "_")
+	url = "https://yande.re/post/index.json?limit={}&tags={}".format(limit, message)
+	response = requests.get(url)
+	data = json.loads(response.text)
+	limit = len(data)
+	if not data:
+		embed=discord.Embed(description = "Couldn't find a picture with that tag", color = color_blue)
+		x = await client.say(embed=embed)
+		await asyncio.sleep(5)
+		return await client.delete_message(x)
+	x = data[random.randint(0, limit-1)]
+	final_url = x["file_url"]
+	embed=discord.Embed(title = "Enjoy {}, ".format(ctx.message.author.name), color = color_blue)
+	embed.set_image(url = final_url)
+	embed.set_footer(text = "From yande.re, Tag: {}, Results found: {}".format(message, limit))
+	await client.say(embed=embed)
+
+@client.command(pass_context=True, no_pm=True, aliases=["dan"])
+@commands.cooldown(3, 5)
+async def danbooru(ctx, *, message:str=None):
+	if ctx.message.channel.is_nsfw == False:
+		embed=discord.Embed(description = "This is not a **nsfw** channel", color = color_blue)
+		x = await client.say(embed=embed)
+		await asyncio.sleep(5)
+		return await client.delete_message(x)
+	limit = 100
+	if message==None:
+		listu = ["breasts", "blush", "skirt", "thighhighs", "large breasts", "underwear", "panties"\
+				"nipples", "ass", "pantyhose", "nude", "pussy"]
+		message = listu[random.randint(0, len(listu)-1)]
+	message = message.replace(" ", "_")
+	url = "https://danbooru.donmai.us/post/index.json?limit={}&tags={}".format(limit, message)
+	response = requests.get(url)
+	data = json.loads(response.text)
+	limit = len(data)
+	if not data:
+		embed=discord.Embed(description = "Couldn't find a picture with that tag", color = color_blue)
+		x = await client.say(embed=embed)
+		await asyncio.sleep(5)
+		return await client.delete_message(x)
+	x = data[random.randint(0, limit-1)]
+	if x["file_url"].startswith("http"):
+		final_url = x["file_url"]
+	else:
+		final_url = "http://danbooru.donmai.us{}".format(x["file_url"])
+	embed=discord.Embed(title = "Enjoy {}, ".format(ctx.message.author.name), color = color_blue)
+	embed.set_image(url = final_url)
+	embed.set_footer(text = "From danbooru, Tag: {}, Results found: {}".format(message, limit))
+	await client.say(embed=embed)
+
+@client.command(pass_context=True, no_pm=True, aliases=["gel"])
+@commands.cooldown(3, 5)
+async def gelbooru(ctx, *, message:str=None):
+	if ctx.message.channel.is_nsfw == False:
+		embed=discord.Embed(description = "This is not a **nsfw** channel", color = color_blue)
+		x = await client.say(embed=embed)
+		await asyncio.sleep(5)
+		return await client.delete_message(x)
+	limit = 100
+	if message==None:
+		listu = ["ass", "breasts", "cameltoe", "long hair", "female", "pussy", "nude", "on bed"]
+		message = listu[random.randint(0, len(listu)-1)]
+	message = message.replace(" ", "_")
+	url = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit={}&tags={}".format(limit,message)
+	response = requests.get(url)
+	data = json.loads(response.text)
+	limit = len(data)
+	if not data:
+		embed=discord.Embed(description = "Couldn't find a picture with that tag", color = color_blue)
+		x = await client.say(embed=embed)
+		await asyncio.sleep(5)
+		return await client.delete_message(x)
+	x = data[random.randint(0, limit-1)]
+	final_url = x["file_url"]
+	embed=discord.Embed(title = "Enjoy {}, ".format(ctx.message.author.name), color = color_blue)
+	embed.set_image(url = final_url)
+	embed.set_footer(text = "From gelbooru, Tag: {}, Results found: {}".format(message, limit))
+	await client.say(embed=embed)
+
+@client.command(pass_context=True, no_pm=True, aliases=["xb"])
+@commands.cooldown(3, 5)
+async def xbooru(ctx, *, message:str=None):
+	if ctx.message.channel.is_nsfw == False:
+		embed=discord.Embed(description = "This is not a **nsfw** channel", color = color_blue)
+		x = await client.say(embed=embed)
+		await asyncio.sleep(5)
+		return await client.delete_message(x)
+	limit = 100
+	if message==None:
+		listu = ["ass", " breasts", "pussy", "female", "nude", "bdsm", "spanking"]
+		message = listu[random.randint(0, len(listu)-1)]
+	message = message.replace(" ", "_")
+	url = "https://xbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit={}&tags={}".format(limit, message)
+	response = requests.get(url)
+	data = json.loads(response.text)
+	limit = len(data)
+	if not data:
+		embed=discord.Embed(description = "Couldn't find a picture with that tag", color = color_blue)
+		x = await client.say(embed=embed)
+		await asyncio.sleep(5)
+		return await client.delete_message(x)
+	x = data[random.randint(0, limit-1)]
+	final_url = "http://img3.xbooru.com/images/{}/{}".format(x["directory"], x["image"])
+	embed=discord.Embed(title = "Enjoy {}, ".format(ctx.message.author.name), color = color_blue)
+	embed.set_image(url = final_url)
+	embed.set_footer(text = "From xbooru, Tag: {}, Results found: {}".format(message, limit))
+	await client.say(embed=embed)
+
+@client.command(pass_context=True, no_pm=True, aliases=["rb"])
+@commands.cooldown(10, 10)
+async def realbooru(ctx, *, message:str=None):
+	if ctx.message.channel.is_nsfw == False:
+		embed=discord.Embed(description = "This is not a **nsfw** channel", color = color_blue)
+		x = await client.say(embed=embed)
+		await asyncio.sleep(5)
+		return await client.delete_message(x)
+	limit = 100
+	if message==None:
+		listu = ["ass", " breasts", "pussy", "female", "nude", "bdsm", "spanking"]
+		message = listu[random.randint(0, len(listu)-1)]
+	message = message.replace(" ", "_")
+	url = "https://realbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit={}&tags={}".format(limit, message)
+	response = requests.get(url)
+	data = json.loads(response.text)
+	limit = len(data)
+	if not data:
+		embed=discord.Embed(description = "Couldn't find a picture with that tag", color = color_blue)
+		x = await client.say(embed=embed)
+		await asyncio.sleep(5)
+		return await client.delete_message(x)
+	x = data[random.randint(0, limit-1)]
+	final_url = "https://realbooru.com/images/{}/{}".format(x["directory"], x["image"])
+	embed=discord.Embed(title = "Enjoy {}, ".format(ctx.message.author.name), color = color_blue)
+	embed.set_image(url = final_url)
+	embed.set_footer(text = "From realbooru, Tag: {}, Results found: {}".format(message, limit))
+	await client.say(embed=embed)
+		
         
 @client.command(pass_context = True)
 async def help(ctx):
@@ -129,7 +309,8 @@ async def help(ctx):
       embed.add_field(name = '!mfmeme',value = 'To see rabdom memes',inline = False)
       embed.add_field(name = '!mfjoke',value ='To see random jokes',inline = False)
       embed.add_field(name = '!mfgifsearch',value ='To search anything and get result as gif image',inline = False)
-      embed.add_field(name = '!mftweet',value ='To tweet anything from any id. Example: ``!mftweet <UserName of twitter> <any text>``',inline = False)
+      embed.add_field(name = '!mfteet',value ='To tweet anything from any id. Example: ``!mftweet <UserName of twitter> <any text>``',inline = False)
+      embed.add_field(name = "NSFW", value = "`!mfrule34`, `!mfyandere`, `!mfdanbooru`, `!mfgelbooru`, `!mfxbooru`, `!mfrealbooru`, `!mfgif`")
       embed.add_field(name = 'Music Commands',value ='``!mfplay``, ``!mfstop``, ``!mfskip``, ``!mfvolume``, ``!mfnp`` and ``!mfqueue``.',inline = False)
       await client.say(embed=embed)
      
